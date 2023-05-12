b689d4370d195a2876210a2de942558078712ff4
ANU <anushree104120@gmail.com>
2023-05-13 00:59:55 +0530

    first

---
+++ clock.js
@@ -0,0 +1,17 @@
+
+function p(){
+      document.getElementById('personal').style.display="block";}
+     
+   
+    
+    
+
+   
+function detail(){
+   var btn = document.getElementById('personal');
+   var date=document.getElementById('date').value;
+   document.getElementById("div6").innerHTML=date;
+ 
+   document.getElementById('details').style.display="block";
+}
+   
